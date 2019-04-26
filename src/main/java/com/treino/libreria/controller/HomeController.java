44460cb609071418415d1ee70f4beb5be2ff6f9d
LucasCarvalhais <lucascarvalhais03@outlook.com>
2019-04-26 14:37:45 -0300

    Primeiro Controller

--- src/main/java/com/treino/libreria/controller/HomeController.java
+++ src/main/java/com/treino/libreria/controller/HomeController.java
@@ -1,17 +1,15 @@
 package com.treino.libreria.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/welcome")
-    public String getWelcome(Model model) {
-        return "welcome";
+    @GetMapping("/welcome")
+    public Model getWelcome(Model model) {
+        return model;
     }
 
 }
