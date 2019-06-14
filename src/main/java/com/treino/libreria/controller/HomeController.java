29b33fb871f97fd64baa3387857cfb96852766a5
LucasCarvalhais <lucascarvalhais03@outlook.com>
2019-06-14 15:34:35 -0300

    REFACTORING: HomeController

--- src/main/java/com/treino/libreria/controller/HomeController.java
+++ src/main/java/com/treino/libreria/controller/HomeController.java
@@ -1,15 +1,14 @@
 package com.treino.libreria.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public ModelAndView getWelcome() {
-        return new ModelAndView("welcome");
+    public String getWelcome() {
+        return "welcome";
     }
 
 }
