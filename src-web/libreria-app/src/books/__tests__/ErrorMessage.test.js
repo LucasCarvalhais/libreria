3fc3e51e8856d2c8f8fb55a127473875842d1cf5
LucasCarvalhais <lucascarvalhais03@outlook.com>
2019-07-05 17:46:57 -0300

    [FE] Inserindo setupTests.js

--- src-web/libreria-app/src/books/__tests__/ErrorMessage.test.js
+++ src-web/libreria-app/src/books/__tests__/ErrorMessage.test.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { render } from 'enzyme';
 import { ErrorMessage } from '../ErrorMessage';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 const error = 'No Network Available';
 
